4ef47616a3903cb7836f6ec405903ecbd9e84d71
tuanteng96 <tuantengyn@gmail.com>
2022-04-18 23:04:58 +0700

    update

--- src/features/WareHouse/components/ItemWareHouse.jsx
+++ src/features/WareHouse/components/ItemWareHouse.jsx
@@ -12,12 +12,12 @@ export default function ItemWareHouse({ item }) {
           {(item.InQty / item.OutQty) * 100}%
         </div>
       </div>
-      <div className="f--1 pl-20px max-w-70">
+      <div className="f--1 pl-20px">
         <div className="font-size-md fw-500 line-height-sm mb-4px text-truncate">
           {item.Title}
         </div>
         <div className="line-height-sm">
-          <span className="pr-2px">Đã dùng {item.InQty}</span>/
+          <span className="pr-2px">Còn lại {item.OutQty - item.InQty}</span>/
           <span className="pl-2px">Tổng {item.OutQty}</span>
         </div>
       </div>
